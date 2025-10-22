3a3785018cc766b4486007352d05cd0ff4a40fd8
Pedro Ivo <pedro019ivo@gmail.com>
2025-10-22 20:40:32 -0300

    mutation AddReactHookFormImportToLoginContainer {   addImport(     filePath: "src/components/LoginContainer.tsx",     importStatement: 'import { useForm } from "react-hook-form";'   ) }

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+
 import { LoginContainer } from "@/components/LoginContainer";
 
 export default function LoginPage() {
   return (
     <section className="w-[90%] h-[90%] bg-gray-200 rounded-2xl shadow-xs shadow-gray-400 flex items-center justify-center">
       <LoginContainer />
+      
     </section>
   );
 }
--- src/components/LoginContainer.tsx
+++ src/components/LoginContainer.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import { useForm } from "react-hook-form";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 export function LoginContainer() {
+  const { register } = useForm();
   return (
     <div className="w-[25rem] h-[34.375rem]  flex flex-col gap-[1rem] p-9 justify-center">
-      <Input placeholder="E-mail" />
+      <Input id="title" title="E-mail" placeholder="E-mail" />
 
-      <Input placeholder="Senha" />
+      <Input id="password" title="Senha" placeholder="Senha" />
 
       <Button>Entrar</Button>
     </div>
