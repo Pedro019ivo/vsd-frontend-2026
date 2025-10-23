f8979c16576b59eb80dd51eb5161e5f1c560d31a
Pedro Ivo <pedro019ivo@gmail.com>
2025-10-22 21:02:37 -0300

    atualizacao

--- src/components/LoginContainer.tsx
+++ src/components/LoginContainer.tsx
@@ -3,14 +3,37 @@
 import { useForm } from "react-hook-form";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import z from "zod";
 
 export function LoginContainer() {
-  const { register } = useForm();
+  const loginForm = z.object({
+    email: z.email(),
+    password: z.string().min(8),
+  });
+
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  console.log(watch());
   return (
     <div className="w-[25rem] h-[34.375rem]  flex flex-col gap-[1rem] p-9 justify-center">
-      <Input id="title" title="E-mail" placeholder="E-mail" />
+      <Input
+        id="title"
+        title="E-mail"
+        placeholder="E-mail"
+        {...register("email")}
+      />
 
-      <Input id="password" title="Senha" placeholder="Senha" />
+      <Input
+        id="password"
+        title="Senha"
+        placeholder="Senha"
+        type="password"
+        {...register("password")}
+      />
 
       <Button>Entrar</Button>
     </div>
